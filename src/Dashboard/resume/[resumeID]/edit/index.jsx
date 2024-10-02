340f44206c3f5c0124e4eeba2e7bd96944dc307c
shekhawatmuskan <muskanshekhawat73@gmail.com>
2024-10-02 22:00:18 +0530

    added all the resume personal detail

--- src/Dashboard/resume/[resumeID]/edit/index.jsx
+++ src/Dashboard/resume/[resumeID]/edit/index.jsx
@@ -7,14 +7,14 @@ import dummy from "@/Data/dummy";
 
 function EditResume() {
   const params = useParams();
-  const [resumeInfo, setResumeInfo] = useState();
+  const [resumeInfo, setResumeInfo] = useState(dummy);
 
   useEffect(() => {
     setResumeInfo(dummy);
   }, []);
 
   return (
-    <ResumeInfoContext.Provider value={{}}>
+    <ResumeInfoContext.Provider value={{ resumeInfo, setResumeInfo }}>
       <div className="grid grid-cols-1 md:grid-cols-2 p-10 gap-10">
         {/* Form Section */}
         <FormSection />
--- src/Dashboard/resume/components/ResumePreview.jsx
+++ src/Dashboard/resume/components/ResumePreview.jsx
@@ -1,20 +1,35 @@
 import { ResumeInfoContext } from "@/context/ResumeInfoContext";
 import React, { useContext } from "react";
+import PersonalDetailPreview from "./preview/PersonalDetailPreview";
+import SummeryPreview from "./preview/SummeryPreview";
+import ExperiencePreview from "./preview/ExperiencePreview";
+import EducationalPreview from "./preview/EducationalPreview";
+import SkillsPreview from "./preview/SkillsPreview";
 
 function ResumePreview() {
   const { resumeInfo, setResumeInfo } = useContext(ResumeInfoContext);
 
   return (
-    <div>
+    <div
+      className="shadow-lg h-full p-14 border-t-[20px]"
+      style={{
+        borderColor: resumeInfo?.themeColor,
+      }}
+    >
       {/**Personal Detail */}
+      <PersonalDetailPreview resumeInfo={resumeInfo} />
 
       {/**Summery */}
+      <SummeryPreview resumeInfo={resumeInfo} />
 
       {/**Professional Experience */}
+      <ExperiencePreview resumeInfo={resumeInfo} />
 
       {/** Educational */}
+      <EducationalPreview resumeInfo={resumeInfo} />
 
       {/** Skills */}
+      <SkillsPreview resumeInfo={resumeInfo} />
     </div>
   );
 }
